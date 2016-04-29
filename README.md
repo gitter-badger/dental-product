8e089cbbfa1aea1779d07fd35062406d60f26c11
prithajnath <prithajnath@gmail.com>
2016-04-28 20:34:51 -0400

    added TODOs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dental-product
 dental products website using mojolicious framework
+TODO:
+
+1) Change MyApp.pm to work with the routes
+2) Register.pl needs to be changed
+3) Login should work with our database instead of the hardcoded users
+4) Make template for the main page, product page
--- myapp/lib/MyApp.pm
+++ myapp/lib/MyApp.pm
@@ -19,18 +19,5 @@ sub startup {
 
     $r->get('/logout')->to('login#logout');
 
-# Lazy connection to the model.
-    helper database => sub { state $database = MyApp::Model::Database->new };
-
-### CONTROLLER ###
-    get '/' => sub { shift->render(template => 'register', error=>'') } => 'register';
-    get '/register' => sub { shift->render(template => 'registered', error=>'') } => 'register';
-    post '/register' => sub { 
-	my $c=shift;
-	my $username=$c->param('username');
-	my $password=$c->param('password');
-	my $email=$c->param('email');
-	my $shipaddress=$c->param('shipaddress');
-}
 }
 1;
